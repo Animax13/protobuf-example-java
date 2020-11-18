10bab5ad2cd1e42ae804a8c0bbf1e9c6b52e9401
Animesh Lal <Animesh.Lal@go-mmt.com>
2020-11-18 15:38:06 +0530

    Simple proto

--- src/main/java/lal/SimpleMain.java
+++ src/main/java/lal/SimpleMain.java
@@ -1,9 +1,8 @@
-package com.github.simplesteph.protobuf;
+package lal;
 
 import example.simple.Simple.SimpleMessage;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
@@ -11,21 +10,19 @@ import java.util.Arrays;
 public class SimpleMain {
 
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        System.out.println("Hello World");
 
         SimpleMessage.Builder builder = SimpleMessage.newBuilder();
 
         // simple fields
-        builder.setId(42)  // set the id field
-                .setIsSimple(true)  // set the is_simple field
-                .setName("My Simple Message Name"); // set the name field
+        builder.setId(42)
+                .setIsSimple(true)
+                .setName("My Simple Message Name");
 
-        // repeated field
+        //repeated field
         builder.addSampleList(1)
                 .addSampleList(2)
-                .addSampleList(3)
-                .addAllSampleList(Arrays.asList(4, 5, 6));
+                .addAllSampleList(Arrays.asList(3, 4, 5));
 
         System.out.println(builder.toString());
 
@@ -36,25 +33,20 @@ public class SimpleMain {
             FileOutputStream outputStream = new FileOutputStream("simple_message.bin");
             message.writeTo(outputStream);
             outputStream.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        // send as byte array
-        // byte[] bytes = message.toByteArray();
+        //send as byte array
+//        byte[] bytes = message.toByteArray();
 
         try {
-            System.out.println("Reading from file... ");
-            FileInputStream fileInputStream = new FileInputStream("simple_message.bin");
-            SimpleMessage messageFromFile = SimpleMessage.parseFrom(fileInputStream);
+            System.out.println("Reading from file...");
+            FileInputStream inputStream = new FileInputStream("simple_message.bin");
+            SimpleMessage messageFromFile = SimpleMessage.parseFrom(inputStream);
             System.out.println(messageFromFile);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
