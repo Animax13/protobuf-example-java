d7d113bd288a996931c24f757fea0d61777dc39f
Animesh Lal <Animesh.Lal@go-mmt.com>
2020-11-18 22:39:31 +0530

    java options

---
+++ src/main/java/lal/OptionsMain.java
@@ -0,0 +1,11 @@
+package lal;
+
+import com.example.options.OptionMessageOther;
+
+public class OptionsMain {
+
+    public static void main(String[] args) {
+
+        OptionMessageOther other = OptionMessageOther.newBuilder().build();
+    }
+}
